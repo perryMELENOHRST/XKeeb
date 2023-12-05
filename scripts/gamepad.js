d8f5c372b44cb60171b249aed1aca7f531e33ed9
Luke G <luke.e.gibson@outlook.com>
2023-12-05 02:49:28 +0000

    gamepad.js start

--- scripts/gamepad.js
+++ scripts/gamepad.js
@@ -1,3 +1,23 @@
+const keys = {
+  KEY_W: "W",
+  KEY_A: "A",
+  KEY_S: "S",
+  KEY_D: "D",
+  KEY_SPACE: "space",
+  KEY_ENTER: "enter",
+}
+const  Keymap = {
+  FORWARD: keys.KEY_W,
+  BACKWARDS: keys.KEY_S,
+  LEFT: keys.KEY_A,
+  RIGHT: keys.KEY_D,
+  SPACE: keys.KEY_SPACE,
+}
+window.addEventListener("keydown", (e)=>{
+  console.log(e.key)
+})  
+
+
 window.addEventListener("gamepadconnected", (e) => {
     console.log(
       "Gamepad connected at index %d: %s. %d buttons, %d axes.",
