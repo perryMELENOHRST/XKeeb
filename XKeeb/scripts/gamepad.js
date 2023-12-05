d49407f7a983fa0cbe3cdfaab2680e8ac7835213
perryMELENOHRST <84308737+perryMELENOHRST@users.noreply.github.com>
2023-12-05 12:27:58 +1100

    XKeeb

---
+++ XKeeb/scripts/gamepad.js
@@ -0,0 +1,9 @@
+window.addEventListener("gamepadconnected", (e) => {
+    console.log(
+      "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index,
+      e.gamepad.id,
+      e.gamepad.buttons.length,
+      e.gamepad.axes.length,
+    );
+  });
